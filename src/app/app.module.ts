c5c24a966f2bb81a433c2e7437d8c604f5838f7c
irishevsu <irish@Irishs-MacBook-Pro.local>
2022-01-08 11:32:55 +0800

    inital commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProfileComponent } from './profile/profile.component';
+import { GalleryComponent } from './gallery/gallery.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactComponent,
+    ProfileComponent,
+    GalleryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
